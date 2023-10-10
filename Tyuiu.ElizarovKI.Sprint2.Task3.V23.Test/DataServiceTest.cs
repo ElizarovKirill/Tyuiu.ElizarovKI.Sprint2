3d01bcaa412a52b84d7811be23cb00fdbc2614c9
ElizarovKirill <144137165+ElizarovKirill@users.noreply.github.com>
2023-10-10 22:02:02 +0500

    Task3

---
+++ Tyuiu.ElizarovKI.Sprint2.Task3.V23.Test/DataServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ElizarovKI.Sprint2.Task3.V23.Lib;
+
+namespace Tyuiu.ElizarovKI.Sprint2.Task3.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidConditiob1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 18;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidConditiob2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = -5.333;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidConditiob3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidConditiob4()
+        {
+            DataService ds = new DataService();
+            double x = -28;
+            double res = ds.Calculate(x);
+            double wait = -308.001;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ElizarovKI.Sprint2.Task3.V23/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ElizarovKI.Sprint2.Task3.V23.Lib;
+
+namespace Tyuiu.ElizarovKI.Sprint2.Task3.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Елизаров К. И. | ИИПб-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Елизаров Кирилл Игоревич | ИИПб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+            Console.WriteLine("* значение переменной X с клавиатуры.                                     *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой;             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+        }
+    }
+}
